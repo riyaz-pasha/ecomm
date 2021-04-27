e5e98600d3d1f58a9320c5dc00c4be237644e5f4
riyaz <riyazpasha880@gmail.com>
2021-04-27 23:14:54 +0530

    refactor products. use local div id in development environment.. expose a function from the ProductsIndex and container will mount this function and render products list

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,5 @@
-import "products/ProductsIndex";
+import { mount as mountProducts } from "products/ProductsIndex";
 import "cart/CartShow";
 
 console.log("Inside Container");
+mountProducts(document.querySelector("#products-list"));
